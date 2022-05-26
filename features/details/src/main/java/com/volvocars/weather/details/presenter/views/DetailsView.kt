13f58a996bd2429b7a7cab7ba08247e7dcfdfc2a
Rasoul Miri <rasoul.miri@solidsport.com>
2022-05-26 15:16:24 +0200

    add icon for DetailsView

--- features/details/src/main/java/com/volvocars/weather/details/presenter/views/DetailsView.kt
+++ features/details/src/main/java/com/volvocars/weather/details/presenter/views/DetailsView.kt
@@ -1,11 +1,18 @@
 package com.volvocars.weather.details.presenter.views
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.volvocars.weather.details.R
 import com.volvocars.weather.details.presenter.navigation.DetailsNavigationModel
 
 @Composable
@@ -19,7 +26,28 @@ fun DetailsView(navigationModel: DetailsNavigationModel?) {
             )
         }
     ) {
-        Text(text = "dddd")
+        Column(modifier = Modifier.padding(8.dp)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(painter = painterResource(R.drawable.ic_thermometer),
+                    contentDescription = null)
+                Text(text = "Temperature: ")
+                Text(text = navigationModel?.temperature ?: "-")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Image(painter = painterResource(R.drawable.ic_thermometer_chevron_down),
+                    contentDescription = null)
+                Text(text = "Minimum Temperature: ")
+                Text(text = navigationModel?.temperatureMin ?: "-")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Image(painter = painterResource(R.drawable.ic_thermometer_chevron_up),
+                    contentDescription = null)
+                Text(text = "Maximum Temperature: ")
+                Text(text = navigationModel?.temperatureMax ?: "-")
+            }
+        }
     }
 }
 
